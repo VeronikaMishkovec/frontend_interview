92a3a95093f8687e768be40de40ab9c99f8b5eb3
vmishkovets <v.mishkovets@belitsoft.com>
2022-11-24 16:32:39 +0300

    ts ignore

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/global.css'
 import { store, wrapper } from '../store/store'
 import { Provider } from 'react-redux'
 
+// eslint-disable-next-line react/prop-types
  function MyApp({ Component, pageProps }) {
     return(
       <Provider store={store}>
