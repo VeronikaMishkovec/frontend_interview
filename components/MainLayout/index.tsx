288fcfc1bec1ab2827425cdeab48b2119f4c4c65
vmishkovets <v.mishkovets@belitsoft.com>
2022-11-29 15:42:57 +0300

    fix errors

--- components/MainLayout/index.tsx
+++ components/MainLayout/index.tsx
@@ -1,20 +1,21 @@
+import React from 'react'
 import { Button } from '@mui/material'
 import Link from 'next/link'
-import React from 'react'
+
+import { MainLayoutType } from './types'
 
 import style from './mainlayout.module.scss'
-import { MainLayoutType } from './types'
 
 export const MainLayout = ({ children, isRegistrationPage }:MainLayoutType) => {
   return (
     <div>
       <header className={style.container}>
         {isRegistrationPage ? (<Link href='/login'>
-          <Button variant='contained' className={style.loginButton}>{'Login'}</Button>
+          <Button className={style.loginButton} variant='contained'>{'Login'}</Button>
         </Link> ): (<Link href="/registration">
             <Button
-              variant='contained'
               className={style.registrationButton}
+              variant='contained'
             >
               {'Registration'}
             </Button>
