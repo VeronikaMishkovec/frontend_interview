f86cd0e39d3ec9c74806a424a069d217aad288ef
vmishkovets <v.mishkovets@belitsoft.com>
2022-12-06 15:08:14 +0300

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/global.css'
 
 // eslint-disable-next-line react/prop-types
- function MyApp({ Component, pageProps }) {
+  function MyApp({ Component, pageProps }) {
     return(
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
 import { MainLayout } from '../components/MainLayout'
 import { LABELS } from "../constants/labels";
-import { CategoryType, MainType } from '../store/types'
 
 import styles from '../styles/home.module.scss';
 
-const Home = ({ list }: MainType['category']) => {
+const Home = () => {
   return (
     <MainLayout isRegistrationPage={true}>
       <div className={styles.container}>
         <div>{LABELS.WELCOME}</div>
-        <div className={styles.categoryListContainer}>
-          {list?.map((category: CategoryType) => {
-            return (
-              <div className={styles.categoryContainer} key={category._id}>
-                <img alt={''} src={category.img}/>
-                <div>{category.title}</div>
-              </div>
-            )
-          })}
-        </div>
       </div>
     </MainLayout>
   )
