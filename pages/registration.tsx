33b3f460cbc469faf0b82d50b805513c7a7210d6
vmishkovets <v.mishkovets@belitsoft.com>
2022-12-06 15:13:54 +0300

    fix deploy error reg page

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { Button, TextField } from '@mui/material'
 
 import { MainLayout } from '../components/MainLayout'
-import { registrationRequestAction } from '../store/reducers/authReducer'
 
 import style from '../styles/registration.module.scss'
 
@@ -11,15 +9,13 @@ const Registration = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const dispatch = useDispatch();
-
   return (
     <MainLayout isRegistrationPage={true}>
       <div className={style.container}>
         <div className={style.pageHeader}>{'Registration'}</div>
         <TextField className={style.inputs} id="outlined-basic" label="Email" value={email} variant="outlined" onChange={(e) => setEmail(e.target.value)} />
         <TextField className={style.inputs} id="outlined-basic" label="Password" type={'password'} value={password} variant="outlined" onChange={(e) => setPassword(e.target.value)}/>
-        <Button className={style.button} variant={'contained'} onClick={() => dispatch(registrationRequestAction({ email, password }))}>{'Submit'}</Button>
+        <Button className={style.button} variant={'contained'} onClick={() => {}}>{'Submit'}</Button>
       </div>
     </MainLayout>
   )
