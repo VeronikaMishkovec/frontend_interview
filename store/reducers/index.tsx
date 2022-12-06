ce3003394a2d409109f26840be27c2286430a17a
vmishkovets <v.mishkovets@belitsoft.com>
2022-12-06 15:10:12 +0300

    remove files

--- store/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import { authSlice } from './authReducer'
-import { categorySlice } from './categoryReducer'
-
-export const rootReducer = combineReducers({
-  auth: authSlice.reducer,
-  category: categorySlice.reducer
-});
--- store/selectors.ts
+++
@@ -1,3 +0,0 @@
-import { MainType } from './types'
-
-export const categoriesListSelector = (state: MainType) => state.category.list
